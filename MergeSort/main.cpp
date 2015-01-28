9cfe6f7344e7c83e5589201191b7fca0b1e67d53
Vinod Gupta <vinod_gupta@live.com>
2015-01-28 15:35:23 -0800

    Initial Commit

---
+++ MergeSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MergeSort
+//
+//  Created by Vinod Gupta on 1/28/15.
+//  Copyright (c) 2015 Vinod Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
